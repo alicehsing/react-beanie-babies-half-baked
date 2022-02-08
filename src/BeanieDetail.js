ecfdece73bf1f8a5c24528e17242d3cd3ae3127e
Alice Hsing <alicehsing@gmail.com>
2022-02-07 16:05:33 -0800

    test passed

--- src/BeanieDetail.js
+++ src/BeanieDetail.js
@@ -15,10 +15,11 @@ export default function BeanieDetail() {
       setBeanieBaby(data);
     }
     fetchBeanieBaby();
-  }, []); // note that you'll want the id from the url in the dependency array because you want the useEffect callback to get called every time the url changes 
+  }, [params.id]); // note that you'll want the id from the url in the dependency array because you want the useEffect callback to get called every time the url changes 
 
   function handleBeanieClick() {
     // here's a challenge. How can you link on click to the beanie baby's correct entry in the official beanie baby fan site?
+    window.location.href === beanieBaby.link;
   }
   
   return (
